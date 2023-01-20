fc5d74cb334159de89291e7908de170988d074b4
ahadb802 <abdulahadb802@gmail.com>
2023-01-21 03:50:14 +0500

    Displaying books in UI

---
+++ src/components/books/books.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './books.css';
+
+const Books = () => (
+  <div className="books-section">
+    <div className="book-info">
+      <div className="description">
+        <h2>Book 1</h2>
+        <h4>Belay</h4>
+      </div>
+      <div className="btn">
+        <button type="button">
+          Comments
+        </button>
+        <button type="button">
+          Remove
+        </button>
+        <button type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div className="book-info">
+      <div className="description">
+        <h2>Book 2</h2>
+        <h4>Ahad</h4>
+      </div>
+      <div className="btn">
+        <button type="button">
+          Comments
+        </button>
+        <button type="button">
+          Remove
+        </button>
+        <button type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div className="book-info">
+      <div className="description">
+        <h2>Book 3</h2>
+        <h4>Diego</h4>
+      </div>
+      <div className="btn">
+        <button type="button">
+          Comments
+        </button>
+        <button type="button">
+          Remove
+        </button>
+        <button type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div className="book-info">
+      <div className="description">
+        <h2>Book 3</h2>
+        <h4>Diego</h4>
+      </div>
+      <div className="btn">
+        <button type="button">
+          Comments
+        </button>
+        <button type="button">
+          Remove
+        </button>
+        <button type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default Books;
