1c371a3996904ca444ae6f5c8b6585a760b0e14e
ahadb802 <abdulahadb802@gmail.com>
2023-01-21 03:52:14 +0500

    added home and categories page

---
+++ src/pages/categories/Categories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './cat.css';
+
+const Categories = () => (
+  <section className="cat-container">
+    <div>
+      <h1>Categories</h1>
+      <p>Status</p>
+      <button
+        type="button"
+      >
+        Check Status
+      </button>
+    </div>
+  </section>
+);
+export default Categories;
