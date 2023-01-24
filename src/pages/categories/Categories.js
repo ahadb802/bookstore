d75c7b7100b81f3e08b609465e32da7792a711c5
ahadb802 <abdulahadb802@gmail.com>
2023-01-25 02:41:33 +0500

    checking status

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
 import './cat.css';
 
-const Categories = () => (
-  <section className="cat-container">
-    <div>
-      <h1>Categories</h1>
-      <p>Status</p>
-      <button
-        type="button"
-      >
-        Check Status
-      </button>
-    </div>
-  </section>
-);
+const Categories = () => {
+  const send = useDispatch();
+  const status = useSelector((state) => state.categories);
+
+  const check = () => send(checkStatus());
+  return (
+    <section className="cat-container">
+      <div>
+        <h1>Categories</h1>
+        <p>{status}</p>
+        <button
+          type="button"
+          onClick={check}
+        >
+          Check Status
+        </button>
+      </div>
+    </section>
+  );
+};
 export default Categories;
