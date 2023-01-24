005f9e51081ee271b8c8b59adf091afebb4c1646
ahadb802 <abdulahadb802@gmail.com>
2023-01-25 02:42:30 +0500

    rendering all UI

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { listBooks } from '../../redux/books/book';
 import Add from '../../components/addBook/addBook';
 import Books from '../../components/books/books';
 
-const Home = () => (
-  <section>
-    <Books />
-    <Add />
-  </section>
+const Home = () => {
+  const booksList = useSelector((state) => state.books);
+  const send = useDispatch();
 
-);
+  useEffect(() => {
+    send(listBooks());
+  }, [send]);
+
+  let display = '';
+  display = booksList.map((item) => (
+    <Books
+      id={item.id}
+      key={item.id}
+      book={item}
+      title={item.title}
+      author={item.author}
+      category={item.category}
+    />
+  ));
+  return (
+    <section>
+      {display}
+      <Add />
+    </section>
+
+  );
+};
 export default Home;
